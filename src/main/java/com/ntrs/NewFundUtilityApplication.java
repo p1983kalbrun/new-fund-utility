a5293049087fc7805a5fcd2c7daa514765d96b24
p1983kalbrun <53264036+p1983kalbrun@users.noreply.github.com>
2019-09-23 14:07:56 +0100

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/main/java/com/ntrs/NewFundUtilityApplication.java
@@ -0,0 +1,12 @@
+package com.ntrs;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewFundUtilityApplication {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(NewFundUtilityApplication.class, args);
+	}
+
+}
